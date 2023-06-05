ba3588123534341957d5e278dcfbae9c5d53cd11
Md Rana Hossain <ranasl62@gmail.com>
2023-06-05 02:34:47 -0500

    Aspect with pointcut ExecutionTime, AfterThrowing and global exception handle and http response added

--- src/main/java/miu/edu/lab/aspect/v1/ExceptionAspect.java
+++ src/main/java/miu/edu/lab/aspect/v1/ExceptionAspect.java
@@ -1,4 +1,34 @@
 package miu.edu.lab.aspect.v1;
 
-public class ExceptionAsoect {
+import miu.edu.lab.domain.v1.ExceptionEntity;
+import miu.edu.lab.service.v1.ExceptionService;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Aspect
+@Component
+public class ExceptionAspect {
+
+    @Autowired
+    private ExceptionService exceptionService;
+
+    @AfterThrowing(pointcut = "execution(* miu.edu.lab..*.*(..))", throwing = "exception")
+    public void handleException(JoinPoint joinPoint, Exception exception) {
+        ExceptionEntity exceptionEntity = new ExceptionEntity();
+        exceptionEntity.setDate(LocalDate.now());
+        exceptionEntity.setTime(LocalTime.now());
+        exceptionEntity.setPrinciple("Faker");
+
+        exceptionEntity.setExceptionType(exception.getClass().getName() + "@" + exception.getClass().getTypeName());
+        exceptionEntity.setExceptionMessage(exception.getMessage());
+
+        exceptionService.create(exceptionEntity);
+    }
 }
+
